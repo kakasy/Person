2309e8b1621d311f04a5d6be99ab0c18176df025
kakasy <37814336+kakasy@users.noreply.github.com>
2018-12-17 19:45:19 +0300

    Update Main.java

--- NetCracker/src/main/java/Main.java
+++ NetCracker/src/main/java/Main.java
@@ -21,12 +21,13 @@ public class Main {
         while(true)
         {
             System.out.print("Опции: \n 0 - Показать всех людей  \n 1 - Показать человека  \n 2 - Добавить человека  " +
-                    "\n 3 - Удалить человека \n");
+                    "\n 3 - Удалить человека \n 4 - Сравнить по id двух людей \n 4 - Выбрать тип сортировки \n 5 - Запись лога");
             int variant = sc.nextInt();
             int id;
             String FIO;
             char gender;
             LocalDate dateOfBirth;
+            PersonComparator pc = new IdPersonComparator();
             switch (variant)
             {
                 case 0:
@@ -58,7 +59,35 @@ public class Main {
                     id = sc.nextInt();
                     people.deletePerson(id);
                     break;
-
+                    
+                case 4:
+                    System.out.print("Выберите тип сортировки: \n 0 - ShakerSort");
+                    variant = sc.nextInt();
+                    System.out.println("Выберите тип поиска: \n 0 - по ФИО \n 1 - по дате \n 2 - по возрасту \n");
+                    type = sc.nextInt();
+                    switch(type)
+                    {
+                        case 0:
+                            pc = new FIOPersonComparator();
+                            break;
+                        case 1:
+                            pc = new DatePersonComparator();
+                            break;
+                        case 2:
+                            pc = new AgePersonComparator();
+                            break;
+                    }
+                    switch(variant)
+                    {
+                        case 0:
+                            // sorter = new ShakerSort();
+                    }
+                    people.sort(pc)
+                        break;
+                case 5:
+                    log.info("Приложение закрыто");
+                    System.exit(0);
+                    
                 default:
                     System.out.println("Неверное число. Попробуй снова");
             }
